c58eee0f3638f09c6a120a97c2ae5347808f2e01
dem0N4k <demON4k1993@gmail.com>
2023-08-22 02:55:24 +0300

    finish

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String CORRECT_CHARS = " абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-";
+
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        boolean isValid = true;
+        int spaceCount = 0;
+
+        System.out.println("Введите ФИО");
+        String name = new Scanner(System.in).nextLine().trim();
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == ' ') {
+                spaceCount++;
+            }
+            if (!CORRECT_CHARS.contains(String.valueOf(name.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid && spaceCount == 2) {
+            System.out.println("Фамилия: " + name.substring(0, name.indexOf(' '))
+                    + "\nИмя:" + name.substring(name.indexOf(' '), name.lastIndexOf(' '))
+                    + "\nОтчество:" + name.substring(0, name.indexOf(' ')));
+        } else {
+            System.out.println("Введенная строка не является ФИО!");
+        }
     }
 }
